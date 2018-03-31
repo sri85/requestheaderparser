22333c342ba453b853bdfdf785ea9385c6fe9ce3
sri85 <sripathi@roxtr.com>
2018-03-31 20:31:05 +0100

    added linting to the project

--- tests/hostinfo.test.js
+++ tests/hostinfo.test.js
@@ -1,73 +1,67 @@
-'use strict';
 
-const chai = require("chai");
-const chaiAsPromised = require("chai-as-promised");
-chai.use(chaiAsPromised);
-const assert = chai.assert;
-const {expect} = require('chai');
 
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const { expect, assert } = require('chai');
 const nock = require('nock');
 const hostinfo = require('../hostInfo');
 
-describe("Host Info test suite", () => {
+chai.use(chaiAsPromised);
 
-    it("getUserLanguage returns a language from a header", () => {
-        expect(hostinfo.getUserLanguage("en-US,en;q=0.9")).to.equal("en-US");
-    });
+describe('Host Info test suite', () => {
+  it('getUserLanguage returns a language from a header', () => {
+    expect(hostinfo.getUserLanguage('en-US,en;q=0.9')).to.equal('en-US');
+  });
 
-    it("getUserLanguage returns undefined if a null is passed as an input", () => {
-        expect(hostinfo.getUserLanguage(null)).to.equal(undefined);
-    });
+  it('getUserLanguage returns undefined if a null is passed as an input', () => {
+    expect(hostinfo.getUserLanguage(null)).to.equal(undefined);
+  });
 
-    it("getUserLanguage returns undefined if an undefined as an input", () => {
-        expect(hostinfo.getUserLanguage(undefined)).to.equal(undefined);
-    });
+  it('getUserLanguage returns undefined if an undefined as an input', () => {
+    expect(hostinfo.getUserLanguage(undefined)).to.equal(undefined);
+  });
 
-    it("getUserLanguage returns empty string if an empty string is passed  as an input", () => {
-        expect(hostinfo.getUserLanguage("")).to.equal(undefined);
-    });
+  it('getUserLanguage returns empty string if an empty string is passed  as an input', () => {
+    expect(hostinfo.getUserLanguage('')).to.equal(undefined);
+  });
 
-    it("getUserOS returns undefined if an null is passed as an input", () => {
-        expect(hostinfo.getUserOS(null)).to.equal(undefined);
-    });
+  it('getUserOS returns undefined if an null is passed as an input', () => {
+    expect(hostinfo.getUserOS(null)).to.equal(undefined);
+  });
 
-    it("getUserOS returns undefined if an undefined is passed as an input", () => {
-        expect(hostinfo.getUserOS(undefined)).to.equal(undefined);
-    });
+  it('getUserOS returns undefined if an undefined is passed as an input', () => {
+    expect(hostinfo.getUserOS(undefined)).to.equal(undefined);
+  });
 
-    it("getUserOS returns undefined if an empty string is passed as an input", () => {
-        expect(hostinfo.getUserOS("")).to.equal(undefined);
-    });
+  it('getUserOS returns undefined if an empty string is passed as an input', () => {
+    expect(hostinfo.getUserOS('')).to.equal(undefined);
+  });
 
-    it("getUserIp returns the IP of the user", () => {
-        const scope = nock('https://api.ipify.org')
-            .get('/?format=json')
-            .reply(200, '{\n' +
+  it('getUserIp returns the IP of the user', () => {
+    nock('https://api.ipify.org')
+      .get('/?format=json')
+      .reply(200, '{\n' +
                 '    "ip": "2.125.30.162"\n' +
                 '}');
-        return assert.eventually.equal(hostinfo.getHostIp(), "2.125.30.162");
+    return assert.eventually.equal(hostinfo.getHostIp(), '2.125.30.162');
+  });
 
-    });
-
-    it("getUserIp returns false when the server is down ", () => {
-        const scope = nock('https://api.ipify.org')
-            .get('/?format=json')
-            .reply(500, '{\n' +
+  it('getUserIp returns false when the server is down ', () => {
+    nock('https://api.ipify.org')
+      .get('/?format=json')
+      .reply(500, '{\n' +
                 '    "ip": "2.125.30.162"\n' +
                 '}');
-
-        return assert.eventually.equal(hostinfo.getHostIp(), "{error:'something went wrong'}");
-    });
-
-    it("getUserIp returns error if the server contract changes ",()=>{
-        const scope = nock('https://api.ipify.org')
-            .get('/?format=json')
-            .reply(200, '{\n' +
-                '    "error": "2.125.30.162"\n' +
-                '}');
-
-    });
 
     return assert.eventually.equal(hostinfo.getHostIp(), "{error:'something went wrong'}");
+  });
 
+  it('getUserIp returns error if the server contract changes ', () => {
+    nock('https://api.ipify.org')
+      .get('/?format=json')
+      .reply(200, '{\n' +
+                '    "error": "2.125.30.162"\n' +
+                '}');
+  });
+  return assert.eventually.equal(hostinfo.getHostIp(), "{error:'something went wrong'}");
 });
--- tests/server.test.js
+++ tests/server.test.js
@@ -1,40 +1,33 @@
-'use strict';
+
 
 const userInfoServer = require('../server');
 
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-let should = chai.should();
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const { expect } = require('chai');
+
+chai.should();
 chai.use(chaiHttp);
-const {expect} = require('chai');
 
-describe("UserInfo test suite", () => {
-    it("returns status code 200", (done) => {
 
-        chai.request(userInfoServer)
-            .get('/api/userinfo')
-            .end((err, res) => {
-                return expect(res).to.have.status(200);
+describe('UserInfo test suite', () => {
+  it('returns status code 200', (done) => {
+    chai.request(userInfoServer)
+      .get('/api/userinfo')
+      .end((err, res) => expect(res).to.have.status(200));
+    done();
+  });
+  it('Checks the content type', (done) => {
+    chai.request(userInfoServer)
+      .get('/api/userinfo')
+      .end((err, res) => expect(res).to.be.json);
+    done();
+  });
 
-            });
-        done();
-    });
-    it("Checks the content type", (done) => {
-
-        chai.request(userInfoServer)
-            .get('/api/userinfo')
-            .end((err, res) => {
-                return expect(res).to.be.json;
-            });
-        done();
-    });
-
-    it("Check for Error", (done) => {
-        chai.request(userInfoServer)
-            .get('/api/userinfo')
-            .end((err, res) => {
-                return expect(res.text).to.equal('{"ipaddress":"{error:\'something went wrong\'}"}');
-            });
-        done();
-    });
+  it('Check for Error', (done) => {
+    chai.request(userInfoServer)
+      .get('/api/userinfo')
+      .end((err, res) => expect(res.text).to.equal('{"ipaddress":"{error:\'something went wrong\'}"}'));
+    done();
+  });
 });
