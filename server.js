1c2b6cddb4e2ab35231351a71076fc328bfdc190
sri85 <sripathi@roxtr.com>
2018-03-31 21:42:32 +0100

    added the static html file and made the port to be passed an env var

--- server.js
+++ server.js
@@ -23,7 +23,14 @@ app.get('/api/userinfo', (request, response) => {
   });
 });
 
-app.listen(3000, (err) => {
+app.route('/')
+  .get((req, res) => {
+    res.sendFile(`${process.cwd()}/views/index.html`);
+  });
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, (err) => {
   if (err) {
     throw err;
   }
